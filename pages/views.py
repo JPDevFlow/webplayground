e711b2457dcee542ff4d7399f4978ad8a066a6e7
JEHYP1021 <jehypo1021@gmail.com>
2025-06-20 09:54:44 +0200

    Se ha agregado el formulario para crear paginas, y el redireccionamiento a la lista de paginas

--- pages/views.py
+++ pages/views.py
@@ -2,6 +2,7 @@ from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from django.views.generic.edit import CreateView
 from .models import Page
+from django.urls import reverse_lazy
 
 # Create your views here.
 class PageListView(ListView):
@@ -15,3 +16,8 @@ class PageDetailView(DetailView):
 class PageCreateView(CreateView):
     model = Page
     fields = ["title", "content", "order"]
+
+    success_url = reverse_lazy("pages:pages")
+
+       
+    
