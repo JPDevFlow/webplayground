3808861b9434b1cfb16a967987a388052edf84e3
JEHYP1021 <jehypo1021@gmail.com>
2025-06-22 11:57:08 +0200

    Agregado actualizacion de paginas

--- pages/views.py
+++ pages/views.py
@@ -1,6 +1,6 @@
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
-from django.views.generic.edit import CreateView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Page
 from django.urls import reverse_lazy
 
@@ -18,6 +18,16 @@ class PageCreateView(CreateView):
     fields = ["title", "content", "order"]
 
     success_url = reverse_lazy("pages:pages")
+
+
+class PageUpdateView(UpdateView):
+    model = Page
+    fields = ["title", "content", "order"]
+    template_name_suffix = "_update_form"
+    def get_success_url(self):
+        return reverse_lazy("pages:update", args=[self.object.id]) + '?ok'
+
+
 
        
     
