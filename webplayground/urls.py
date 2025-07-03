af2871ab3c5519c85278a3d3ff209115d0235433
JEHYP1021 <jehypo1021@gmail.com>
2025-07-03 11:53:19 +0200

    registro con CBV

--- webplayground/urls.py
+++ webplayground/urls.py
@@ -24,4 +24,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     #paths de auth
     path('accounts/', include('django.contrib.auth.urls')),
+     path('accounts/', include('registration.urls')),
 ]
