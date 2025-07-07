9dd71f52fa8d4f4a3e19961130b7ffa2ce10573d
JEHYP1021 <jehypo1021@gmail.com>
2025-07-07 10:19:26 +0200

    Agregado formulario de registro

--- registration/views.py
+++ registration/views.py
@@ -6,5 +6,7 @@ from django.urls import reverse_lazy
 
 class SignUpView(CreateView):
     form_class = UserCreationForm
-    success_url = reverse_lazy('login')  # Redireccionar a la página de inicio de sesión después del registro exitoso
     template_name = 'registration/signup.html'  
+
+    def get_success_url(self):
+        return reverse_lazy('login') +'?register' # Redireccionar a la página de inicio de sesión después del registro exitoso
