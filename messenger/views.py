a4977ebb4554854514e10d69e5ee8082e0297de8
JEHYP1021 <jehypo1021@gmail.com>
2025-07-25 11:34:25 +0200

    añadido metodo decorador

--- messenger/views.py
+++ messenger/views.py
@@ -2,8 +2,11 @@ from django.shortcuts import render
 from django.views.generic.list import ListView
 from django.views.generic.detail import DetailView
 from .models import Thread
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 # Create your views here.
 
+@method_decorator(login_required, name='dispatch')
 class ThreadList(ListView):
     model = Thread
 
